1d50880dbdb9535da5497ceda9e15afa787eb80c
LuIsMoReNo7 <1483853@senati.pe>
2023-03-13 18:19:25 -0500

    Practicando con java, dia lunes

---
+++ Practica_dia_lunes/src/Probando_valores_entendibles.java
@@ -0,0 +1,10 @@
+public class Probando_valores_entendibles {
+    public static void main(String[] args) {
+      //vamos bien
+      int minutosporhora = 60;
+      // Está bien, pero no es tan fácil entender qué es m en realidad
+      int m = 60;
+      System.out.println(minutosporhora);
+      System.out.println(m); 
+    }
+}
